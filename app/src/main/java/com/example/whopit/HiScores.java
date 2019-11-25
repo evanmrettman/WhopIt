691b979a5fe4aa0b5ba0b09fd3329edba4c9e029
Evan Rettman <evanmrettman@gmail.com>
2019-11-25 12:45:09 -0500

    created activities

---
+++ app/src/main/java/com/example/whopit/HiScores.java
@@ -0,0 +1,14 @@
+package com.example.whopit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HiScores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hi_scores);
+    }
+}
--- app/src/main/java/com/example/whopit/WhopIt.java
+++ app/src/main/java/com/example/whopit/WhopIt.java
@@ -1,13 +1,14 @@
 package com.example.whopit;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class WhopIt extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_whop_it);
     }
 }
