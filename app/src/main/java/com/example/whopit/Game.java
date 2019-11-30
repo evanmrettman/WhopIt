b37f93e0590facdcca3884a2f3f88893aa14fdf3
Evan Rettman <erettman@masonlive.gmu.edu>
2019-11-30 17:48:11 -0500

    game class

---
+++ app/src/main/java/com/example/whopit/Game.java
@@ -0,0 +1,27 @@
+package com.example.whopit;
+
+import java.util.Locale;
+
+public class Game implements Comparable<Game>
+{
+    int num = -1;
+    int score = 0;
+
+    public Game(int num,int score)
+    {
+        this.num = num;
+        this.score = score;
+    }
+
+    @Override
+    public int compareTo(Game game)
+    {
+        return Integer.compare(this.score,game.score);
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format(Locale.getDefault(),"Game #%2d: %3d",this.num,this.score);
+    }
+}
