7dde2142bc710a950b291897d4c066ff09933ecc
Evan Rettman <erettman@masonlive.gmu.edu>
2019-11-30 17:55:03 -0500

    most interactions with sub-activities

--- app/src/main/java/com/example/whopit/Main.java
+++ app/src/main/java/com/example/whopit/Main.java
@@ -3,17 +3,37 @@ package com.example.whopit;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main extends AppCompatActivity {
+
+    final static public String[] COLUMNS = {"_id","game","score"};
+    final static public String TABLE = "scores";
+    private Integer gamesPlayed = 0;
+    private ArrayList<Game> games;
+    private SQLiteDatabase db;
+    private DatabaseOpenHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new DatabaseOpenHelper(getApplicationContext());
+        db = dbHelper.getWritableDatabase();
+        updateList();
+        Log.i("games_main",games.toString());
+
         ((Button) findViewById(R.id.but_start)).setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -50,7 +70,28 @@ public class Main extends AppCompatActivity {
         {
             if(data == null)
                 return;
-            Integer high_score = data.getIntExtra("score",0);
+            ContentValues cv = new ContentValues();
+            cv.put(COLUMNS[1],data.getIntExtra("game",-1));
+            cv.put(COLUMNS[2],data.getIntExtra("score",0));
+            db.insert(TABLE,null,cv);
+            updateList();
         }
     }
+
+    private void updateList()
+    {
+        games = new ArrayList<Game>();
+        Cursor c = db.query(TABLE,COLUMNS,null,null,null,null,null);
+        for(c.moveToFirst(); !c.isAfterLast(); c.moveToNext())
+        {
+            int curr_game = c.getInt(1);
+            int curr_score = c.getInt(2);
+            if(gamesPlayed < curr_game)
+                gamesPlayed = curr_game;
+            games.add(new Game(curr_game,curr_score));
+        }
+        c.close();
+        Collections.sort(games);
+    }
+
 }
